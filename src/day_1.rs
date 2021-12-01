5e29d35b4ff59acdf5a6d1d2b46d0ade015d3332
miolfo <mikko.o.forsman@gmail.com>
2021-12-01 19:37:12 +0200

    Day 1 part 2 solution

--- src/day_1.rs
+++ src/day_1.rs
@@ -8,5 +8,22 @@ pub fn count_measurement_increases(input: Vec<String>) {
         }
         prev = current;
     }
-    println!("Number increased {} times", increases)
+    println!("Number increased {} times", increases);
+}
+
+pub fn count_measurement_increases_sliding(input: Vec<String>) {
+    let mut increases: i32 = 0;
+    let mut prev_sum: i32 = -1;
+    for i in 1..input.len() - 1 {
+        let sum = to_i32(&input[i-1]) + to_i32(&input[i]) + to_i32(&input[i+1]);
+        if prev_sum != -1 && sum > prev_sum {
+            increases += 1
+        }
+        prev_sum = sum
+    }
+    println!("Sliding sum increased {} times", increases);
+}
+
+fn to_i32(string: &str) -> i32 {
+    string.parse().unwrap_or_default()
 }
--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,5 @@ mod util;
 
 fn main() {
     day_1::count_measurement_increases(util::read_resource("day_1.txt"));
+    day_1::count_measurement_increases_sliding(util::read_resource("day_1.txt"));
 }
