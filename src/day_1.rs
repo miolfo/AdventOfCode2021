ecc81451226604ec7451c185285b545c7cdbd630
miolfo <mikko.o.forsman@gmail.com>
2021-12-01 19:20:08 +0200

    Day 1 part 1 solution

--- src/day_1.rs
+++ src/day_1.rs
@@ -1,3 +1,12 @@
 pub fn count_measurement_increases(input: Vec<String>) {
-
+    let mut increases: i32 = 0;
+    let mut prev: i32 = -1;
+    for line in input {
+        let current: i32 = line.parse().unwrap_or_default();
+        if prev != -1 && current > prev {
+            increases += 1;
+        }
+        prev = current;
+    }
+    println!("Number increased {} times", increases)
 }
